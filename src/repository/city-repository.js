9cb29bad7b8cc920c4d00a74fa227bdc47b2399f
Mithalesh kumar <mithaleshkumar852155@gmail.com>
2024-08-15 17:52:32 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -6,6 +6,7 @@ class CityRepository{
         try {
             const city = await City.create({name} );
         } catch (error) {
+            console.log("something went wrong in the repository layer")
             throw {error}
         }
     }
@@ -17,6 +18,30 @@ class CityRepository{
                 }
             })
         } catch (error) {
+            console.log("something went wrong in the repository layer")
+            throw {error}
+        }
+    }
+    async findCity ({cityId}){
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in the repository layer")
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId,data) {
+        try {
+            const city = City.update(data, {
+                where: {
+                    id:cityId
+                }
+            })
+            
+        } catch (error) {
+            console.log("something went wrong in the repository layer")
             throw {error}
         }
     }
